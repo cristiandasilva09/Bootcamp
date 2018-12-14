b2ea28619a843b9b64a2b37b2f849020f79c540d
Cristian D <chaosdragonpvp94@gmail.com>
2018-12-14 18:06:14 -0300

    UserApi
    Api User with Swagger

---
+++ Topic 5 Spring (Core, Boot) and Swagger/usrApi/src/main/java/com/globant/bootcamp/userApi/Swagger/SwaggerConfig.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.globant.bootcamp.userApi.Swagger;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import static springfox.documentation.builders.PathSelectors.regex;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ *
+ * @author Cristian
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+      @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("user")
+                .apiInfo(apiInfo())
+                .directModelSubstitute(LocalDateTime.class, Date.class)
+                .select()
+                .paths(regex("/user.*"))
+                .build();
+    }
+ 
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Infos REST api")
+                .description("PoC of a REST api, Infos")
+                .termsOfServiceUrl("http://en.wikipedia.org/wiki/Terms_of_service")
+                .version("2.0")
+                .build();
+    }
+}
